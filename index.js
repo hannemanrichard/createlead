9146bfe57f14845282b0e9d455fc3c3a6c6c4390
hannemanrichard7 <hannemanrichard7@gmail.com>
2024-07-19 19:24:19 +0100

    datastream first commit

--- index.js
+++ index.js
@@ -29,101 +29,124 @@ function changeTimeZone(date, timeZone) {
   );
 }
 
+function formatDateTime() {
+  const date = new Date();
+
+  // Get date parts
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+
+  // Get time parts
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  const seconds = String(date.getSeconds()).padStart(2, '0');
+
+  // Format date and time
+  const formattedDate = `${year}-${month}-${day}`;
+  const formattedTime = `${hours}:${minutes}:${seconds}`;
+
+  return `${formattedDate} ${formattedTime}`;
+}
+
 const start = async () => {
   console.log("running a task on schedule");
   try {
     // Retrieve parcels payment_status=not-ready&
-    const parcelsResponse = await axios.get(
-      `https://api.yalidine.app/v1/parcels/?order_by=date_last_status&page_size=50`,
-      {
-        headers: {
-          "X-API-ID": "92129974643421801058",
-          "X-API-TOKEN":
-            "JyWPYR7SpCZlWMSO4rQKcqrPAzNwmftAMdv49z07EVtwlTU3aBVNaFF2GLes2uoH",
-        },
+    let parcels = []
+    let arr = []
+    for (let i = 1; i < 22; i++) {
+      const parcelsResponse = await axios.get(
+          `https://app.conexlog-dz.com/api/v1/get/orders?api_token=9bwwR9RRZfUCcNtjnu4IrQyy3DULSohTiWg2ydQZMLGxh21DrapIueF3FCk5&page=${i}`,
+      );
+      console.log(`API called: ${i}`)
+      parcelsResponse.data.data.forEach((parcel) => {
+          if (parcel.status) {
+            const deliveredStatus = ['encaissé_non_payé', 'livré_non_encaissé', 'payé_et_archivé', 'paiements_prets']
+            const returnedStatus = ['retour_chez_livreur', 'retour_transit_entrepot', 'retour_en_traitement', 'retour_recu', 'retour_archive', 'annule']
+
+            let orderStatus;
+            if (deliveredStatus.includes(parcel.status)) {
+              orderStatus = 'delivered'
+            }
+            else if (returnedStatus.includes(parcel.status)) {
+              orderStatus = 'returned'
+            } else if (parcel.status === 'prete_a_expedier') {
+              orderStatus = 'initial'
+            } else {
+              orderStatus = 'processing'
+            }
+
+
+
+            if (!!orderStatus) {
+              parcels.push({
+                tracking: parcel.tracking,
+                last_status: parcel.status,
+                status: orderStatus
+              })
+            } else {
+              parcels.push({
+                tracking: parcel.tracking,
+                last_status: parcel.status
+              })
+            }
+
+          }}
+      );
+    }
+
+    const currentDate = formatDateTime();
+
+    for (const parcel of parcels) {
+      const index = parcels.indexOf(parcel);
+      const {data, error} = await supabase
+          .from('orders')
+          .select()
+          .eq('tracking_id', parcel.tracking)
+          .single()
+
+      if (data && data.last_status !== parcel.last_status) {
+        console.log('in')
+        let dataOrder, errorOrder;
+        if (data.status !== 'delivered' && data.status !== 'returned') {
+          if (data.status !== parcel.status) {
+            const {data: dataLocal, error: errorLocal} = await supabase
+                .from('orders')
+                .update({
+                  dc_recent_status: parcel.last_status,
+                  modified_at: currentDate,
+                  status: parcel.status
+                })
+                .eq('tracking_id', parcel.tracking)
+                .select()
+            dataOrder = dataLocal;
+            errorOrder = errorLocal;
+          } else {
+            const {data: dataLocal, error: errorLocal} = await supabase
+                .from('orders')
+                .update({
+                  dc_recent_status: parcel.last_status,
+                })
+                .eq('tracking_id', parcel.tracking)
+                .select()
+            dataOrder = dataLocal;
+            errorOrder = errorLocal;
+          }
+
+
+          if (data) {
+            console.log(`${parcel.tracking} successfully modified! from ${data.last_status} to ${parcel.last_status}`)
+          }
+
+          if (error) {
+            console.log(`${parcel.tracking} error occured!`)
+          }
+        }
       }
-    );
-    const parcels = parcelsResponse.data.data.map((parcel) => ({
-      tracking: parcel.tracking,
-      first_name: parcel.firstname,
-      last_name: parcel.familyname,
-      created_at: new Date(parcel.date_creation),
-      date_last_status: new Date(parcel.date_last_status),
-      phone: parcel.contact_phone,
-      wilaya: parcel.to_wilaya_name,
-      commune: parcel.to_commune_name,
-      center: parcel.current_center_name,
-      product: parcel.product_list,
-      last_status: parcel.last_status,
-      address: parcel.address,
-      payment_status: parcel.payment_status,
-      tracker_id: 16 /* +parcel.order_id.replace("order_", "") */,
-      is_stopdesk: !!parcel.stopdesk_id,
-      price: parcel.price,
-      delivery_fee: parcel.delivery_fee,
-    }));
-
-
-
-    // Extract tracking numbers
-    const trackings = parcels.map((parcel) => parcel.tracking);
-    const trackingsStr = trackings.join(",");
-    console.log(trackingsStr);
-    const historiesResponse = await axios.get(
-      `https://api.yalidine.app/v1/histories/?tracking=${trackingsStr}&page_size=1000`,
-      {
-        headers: {
-          "X-API-ID": "92129974643421801058",
-          "X-API-TOKEN":
-            "JyWPYR7SpCZlWMSO4rQKcqrPAzNwmftAMdv49z07EVtwlTU3aBVNaFF2GLes2uoH",
-        },
-      }
-    );
-    // console.log(historiesResponse.data.data);
-    const histories = historiesResponse.data.data.map((history) => ({
-      tracking: history.tracking,
-      created_at: history.date_status,
-      status: history.status,
-      reason: history.reason,
-      center_name: history.center_name,
-      wilaya_name: history.wilaya_name,
-      commune_name: history.commune_name,
-    }));
-
-    const keys = ["created_at", "tracking"];
-    const filteredHistories = histories.filter(
-      (
-        (s) => (o) =>
-          ((k) => !s.has(k) && s.add(k))(keys.map((k) => o[k]).join("|"))
-      )(new Set())
-    );
-    let dataf = JSON.stringify(filteredHistories);
-    fs.writeFileSync("histories.json", dataf);
-    const { data, error } = await supabase
-      .from("parcels")
-      .upsert(parcels)
-      .select();
-
-    if (data) {
-      console.log("success");
     }
 
-    if (error) {
-      console.log(error);
-    }
 
-    const { data: dataHistories, error: errorHistories } = await supabase
-      .from("histories")
-      .upsert(filteredHistories)
-      .select();
-
-    if (dataHistories) {
-      console.log("histories upserted");
-    }
-
-    if (errorHistories) {
-      console.log(errorHistories);
-    }
   } catch (error) {
     console.log(error);
   }
